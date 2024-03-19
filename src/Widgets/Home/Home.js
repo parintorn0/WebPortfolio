e398f72076510723176b79925aa27cc5f7943818
parintorn0 <thammasitboonpt@gmail.com>
2024-03-19 18:12:01 +0700

    1

--- src/Widgets/Home/Home.js
+++ src/Widgets/Home/Home.js
@@ -1,7 +1,19 @@
-import HomeBackground from "./HomeBackground";
-
 function Home(){
     return HomeBackground();
+}
+
+function HomeBackground(){
+    return(
+        <h1>
+            <div className="hometitle">
+                <div>PARINTORN</div>
+                <div>THAMAMSITBOON</div>
+                <div>PORTFOLIO</div>
+            </div>
+            <div className="blacktheme"></div>
+            <img src="././images/home_background.jpg" alt="" className="background" />
+        </h1>
+    )
 }
 
 export default Home;
